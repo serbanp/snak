6bb3413c981e211875159f9a45293221fd66f504
GaidurBogdan <bgaidur@gmail.com>
2017-01-11 21:03:05 +0200

    Add files via upload
    Snake, jucator vs calculator.
    W spawneaza calculator
    S spawneaza jucator.
    sau invers nu mai stiu

--- main.cpp
+++ main.cpp
@@ -4,16 +4,17 @@
 using namespace sf;
 
 int size=16;
-int length=60;
-int width=60;
+int length=40;
+int width=40;
 
-int dir=4,dx[5]={1,0,-1,0,0},dy[5]={0,-1,0,1,0},viteza=0.1,num=1;
+int dx[5]={1,0,-1,0,0},dy[5]={0,-1,0,1,0};
+int viteza=100;
+int num1=0,num2=0,dir1=4,dir2=4;
 
 int modulo(int a, int b)
 {
     return (a%b+b)%b;
 }
-
 
 struct sarpe{
     int x,y;
@@ -22,7 +23,8 @@ struct sarpe{
 struct mancare{
     int x,y;
 } m;
-sarpe s[10000];
+
+sarpe s1[10000],s2[10000];
 
 void FaMancare(mancare &m)
 {
@@ -32,8 +34,14 @@ void FaMancare(mancare &m)
         m.x=abs(rand())%length;
         m.y=abs(rand())%width;
         ok = 1;
-        for (int i=0;i<num;i++)
-         if (s[i].x==m.x && s[i].y==m.y)
+        for (int i=0;i<num1;i++)
+         if (s1[i].x==m.x && s1[i].y==m.y)
+          {
+            ok = 0;
+            break;
+          }
+        for (int i=0;i<num1;i++)
+         if (s2[i].x==m.x && s2[i].y==m.y)
           {
             ok = 0;
             break;
@@ -44,16 +52,14 @@ void FaMancare(mancare &m)
 
 int inteligent()
 {
-    int  prim,ultim,i,coada[2][10500]={0},x,y,mat[200][200]={0},moveAI[100000]={0},nrMovesAI;
+    int  prim=0,ultim=0,i,coada[2][10500]={0},x,y,mat[200][200]={0},moveAI[100000]={0},nrMovesAI;
 
-    coada[0][0]=s[0].x;
-    coada[1][0]=s[0].y;
+    coada[0][0]=s2[0].x;
+    coada[1][0]=s2[0].y;
 
-    for (i=1;i<num;i++)
-     mat[s[i].x][s[i].y] = -1;
+    for (i=1;i<num2;i++)
+     mat[s2[i].x][s2[i].y] = -1;
 
-    prim=0;
-    ultim=0;
     while (prim<=ultim)
     {
         for (i=0;i<4;i++)
@@ -71,21 +77,23 @@ int inteligent()
         if(mat[m.x][m.y]>0) break;
         prim++;
     }
+
     x=m.x;
     y=m.y;
     if(mat[m.x][m.y]==0)
     {
       for (i=0;i<4;i++)
-        if (mat[modulo(s[0].x + dx[i],length)][modulo(s[0].y + dy[i],width)] != -1) return i;
+        if (mat[modulo(s2[0].x + dx[i],length)][modulo(s2[0].y + dy[i],width)] != -1) return i;
       return 0;
     }
+
     nrMovesAI = 0;
-    while (x!=s[0].x || y!=s[0].y)
+    while (x!=s2[0].x || y!=s2[0].y)
     {
         if (mat[x][y]==1)
         {
          for (i=0;i<4;i++)
-          if ( modulo(x+dx[i],length)==s[0].x && modulo(y+dy[i],width)==s[0].y )
+          if ( modulo(x+dx[i],length)==s2[0].x && modulo(y+dy[i],width)==s2[0].y )
           {
             nrMovesAI++;
             moveAI[nrMovesAI] = (i+2)%4;
@@ -112,36 +120,64 @@ int inteligent()
 
 void Miscare()
 {
-    for (int i=num;i>0;i--)
+    for (int i=num1;i>0;i--)
     {
-        s[i].x=s[i-1].x;
-        s[i].y=s[i-1].y;
+        s1[i].x=s1[i-1].x;
+        s1[i].y=s1[i-1].y;
     }
-    s[0].x += dx[dir];
-    s[0].y += dy[dir];
+    s1[0].x += dx[dir1];
+    s1[0].y += dy[dir1];
 
-    if (s[0].x>=length) s[0].x=0;
-    if (s[0].x<0) s[0].x=length-1;
-    if (s[0].y>=width) s[0].y=0;
-    if (s[0].y<0) s[0].y=width-1;
+    if (s1[0].x>=length) s1[0].x=0;
+    if (s1[0].x<0) s1[0].x=length-1;
+    if (s1[0].y>=width) s1[0].y=0;
+    if (s1[0].y<0) s1[0].y=width-1;
 
-    if (s[0].x==m.x && s[0].y==m.y)
+    for (int i=num2;i>0;i--)
     {
-        num++;
+        s2[i].x=s2[i-1].x;
+        s2[i].y=s2[i-1].y;
+    }
+    s2[0].x += dx[dir2];
+    s2[0].y += dy[dir2];
+
+    if (s2[0].x>=length) s2[0].x=0;
+    if (s2[0].x<0) s2[0].x=length-1;
+    if (s2[0].y>=width) s2[0].y=0;
+    if (s2[0].y<0) s2[0].y=width-1;
+
+    if (s1[0].x==m.x && s1[0].y==m.y)
+    {
+        num1++;
+        FaMancare(m);
+    }
+    else
+     if (s2[0].x==m.x && s2[0].y==m.y)
+    {
+        num2++;
         FaMancare(m);
     }
 
     //inteligent
-    std::cout<<m.x<<' '<<m.y<<' '<<s[0].x<<' '<<s[0].y<<' '<<(-4)%10<<'\n';
-    dir=inteligent();
+    if (num2>0) dir2=inteligent();
 }
 
+void AI()
+{
+    s2[0].x=abs(rand())%length;
+    s2[0].y=abs(rand())%width;
+    num2=1;
+}
+
+void Player()
+{
+    s1[0].x=abs(rand())%length;
+    s1[0].y=abs(rand())%width;
+    num1=1;
+}
 
 int main()
 {
-    srand(time(NULL));
-    s[0].x=abs(rand())%length;
-    s[0].y=abs(rand())%width;
     sf::RenderWindow window(sf::VideoMode(size*length, size*width), "Snake");
 
     Texture t1,t2,t3;
@@ -153,12 +189,11 @@ int main()
 	Sprite patratSarpe(t2);
 	Sprite patratMancare(t3);
 
+	srand(time(NULL));
+
     sf:Clock clock;
     m.x=abs(rand())%length;
     m.y=abs(rand())%width;
-    //inteligent
-    dir=inteligent();
-
 
     while (window.isOpen())
     {
@@ -169,10 +204,12 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-        if (Keyboard::isKeyPressed(Keyboard::Down)) dir=3;
-        if (Keyboard::isKeyPressed(Keyboard::Left)) dir=2;
-        if (Keyboard::isKeyPressed(Keyboard::Up)) dir=1;
-	    if (Keyboard::isKeyPressed(Keyboard::Right))  dir=0;
+        if (Keyboard::isKeyPressed(Keyboard::W)) AI();
+        if (Keyboard::isKeyPressed(Keyboard::S)) Player();
+        if (Keyboard::isKeyPressed(Keyboard::Down)) dir1=3;
+        if (Keyboard::isKeyPressed(Keyboard::Left)) dir1=2;
+        if (Keyboard::isKeyPressed(Keyboard::Up)) dir1=1;
+	    if (Keyboard::isKeyPressed(Keyboard::Right))  dir1=0;
 
         if (timp>viteza)
         {
@@ -188,16 +225,30 @@ int main()
 		    window.draw(patratAlb);
           }
 
-        for (int i=0;i<num;i++)
+
+        //DrawAI(window);
+        for (int i=0;i<num2;i++)
 	     {
-	        patratSarpe.setPosition(s[i].x*size, s[i].y*size);
+	        patratSarpe.setPosition(s2[i].x*size, s2[i].y*size);
 	        window.draw(patratSarpe);
          }
 
+        for (int i=1;i<num2;i++)
+         if (s2[i].x==s2[0].x && s2[i].y==s2[0].y) window.close();
+
+
+        //DrawPlayer(window);
+        for (int i=0;i<num1;i++)
+	     {
+	        patratSarpe.setPosition(s1[i].x*size, s1[i].y*size);
+	        window.draw(patratSarpe);
+         }
+
+
+        for (int i=1;i<num1;i++)
+         if (s1[i].x==s1[0].x && s1[i].y==s1[0].y) window.close();
         patratMancare.setPosition(m.x*size,  m.y*size);
         window.draw(patratMancare);
-        for (int i=1;i<num;i++)
-         if (s[i].x==s[0].x && s[i].y==s[0].y) window.close();
 
         window.display();
     }
