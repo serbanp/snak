3eee4c805003cf8aeace16c35ae2dfafa2d55d2f
GaidurBogdan <bgaidur@gmail.com>
2017-01-11 18:38:54 +0200

    Add files via upload
    Snake normal si inteligent,fara meniu, merge si prin pereti.
    Inca mai da crash la dimensiuni mari ale sarpelui,nu stiu de ce,dar mult mai rar decat inainte.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,15 @@
 using namespace sf;
 
 int size=16;
-int length=20;
-int width=40;
+int length=60;
+int width=60;
 
-int dir=4,dx[5]={1,0,-1,0,0},dy[5]={0,-1,0,1,0},viteza=5,num=1;
+int dir=4,dx[5]={1,0,-1,0,0},dy[5]={0,-1,0,1,0},viteza=0.1,num=1;
+
+int modulo(int a, int b)
+{
+    return (a%b+b)%b;
+}
 
 
 struct sarpe{
@@ -17,25 +22,25 @@ struct sarpe{
 struct mancare{
     int x,y;
 } m;
-sarpe s[100];
+sarpe s[10000];
 
 void FaMancare(mancare &m)
 {
     int ok=0;
     while (ok == 0)
     {
-        m.x=rand()%length;
-        m.y=rand()%width;
+        m.x=abs(rand())%length;
+        m.y=abs(rand())%width;
         ok = 1;
-        for (int i=0;i<num-1;i++)
+        for (int i=0;i<num;i++)
          if (s[i].x==m.x && s[i].y==m.y)
           {
             ok = 0;
             break;
           }
     }
-    std::cout<<m.x<<' '<<m.y;
 }
+
 
 int inteligent()
 {
@@ -53,11 +58,11 @@ int inteligent()
     {
         for (i=0;i<4;i++)
         {
-            x = coada[0][prim] + dx[i];
-            y = coada[1][prim] + dy[i];
-            if (mat[x][y]==0 && x>=0 && x<length && y>=0 && y<width)
+            x = modulo(coada[0][prim] + dx[i],length);
+            y = modulo(coada[1][prim] + dy[i],width);
+            if (mat[x][y]==0)// && x>=0 && x<length && y>=0 && y<width)
             {
-                mat[x][y]=mat[x-dx[i]][y-dy[i]] + 1;
+                mat[x][y]=mat[modulo(x-dx[i],length)][modulo(y-dy[i],width)] + 1;
                 ultim++;
                 coada[0][ultim] = x;
                 coada[1][ultim] = y;
@@ -71,7 +76,7 @@ int inteligent()
     if(mat[m.x][m.y]==0)
     {
       for (i=0;i<4;i++)
-        if (mat[s[0].x + dx[i]][s[0].y + dy[i]] != -1) return i;
+        if (mat[modulo(s[0].x + dx[i],length)][modulo(s[0].y + dy[i],width)] != -1) return i;
       return 0;
     }
     nrMovesAI = 0;
@@ -80,23 +85,23 @@ int inteligent()
         if (mat[x][y]==1)
         {
          for (i=0;i<4;i++)
-          if ( x+dx[i]==s[0].x && y+dy[i]==s[0].y )
+          if ( modulo(x+dx[i],length)==s[0].x && modulo(y+dy[i],width)==s[0].y )
           {
             nrMovesAI++;
             moveAI[nrMovesAI] = (i+2)%4;
-            x=x+dx[i];
-            y=y+dy[i];
+            x=modulo(x+dx[i],length);
+            y=modulo(y+dy[i],width);
             break;
           }
         }
         else
         for (i=0;i<4;i++)
-         if ( mat[x+dx[i]][y+dy[i]] + 1 == mat[x][y] )
+         if ( mat[modulo(x+dx[i],length)][modulo(y+dy[i],width)] + 1 == mat[x][y] )
         {
             nrMovesAI++;
             moveAI[nrMovesAI] = (i+2)%4;
-            x=x+dx[i];
-            y=y+dy[i];
+            x=modulo(x+dx[i],length);
+            y=modulo(y+dy[i],width);
             break;
         }
     }
@@ -114,17 +119,20 @@ void Miscare()
     }
     s[0].x += dx[dir];
     s[0].y += dy[dir];
+
+    if (s[0].x>=length) s[0].x=0;
+    if (s[0].x<0) s[0].x=length-1;
+    if (s[0].y>=width) s[0].y=0;
+    if (s[0].y<0) s[0].y=width-1;
+
     if (s[0].x==m.x && s[0].y==m.y)
     {
         num++;
         FaMancare(m);
     }
 
-    if (s[0].x>=length) s[0].x=0;
-    if (s[0].x<0) s[0].x=length-1;
-    if (s[0].y>=width) s[0].y=0;
-    if (s[0].y<0) s[0].y=width-1;
     //inteligent
+    std::cout<<m.x<<' '<<m.y<<' '<<s[0].x<<' '<<s[0].y<<' '<<(-4)%10<<'\n';
     dir=inteligent();
 }
 
@@ -132,8 +140,8 @@ void Miscare()
 int main()
 {
     srand(time(NULL));
-    s[0].x=rand()%length;
-    s[0].y=rand()%width;
+    s[0].x=abs(rand())%length;
+    s[0].y=abs(rand())%width;
     sf::RenderWindow window(sf::VideoMode(size*length, size*width), "Snake");
 
     Texture t1,t2,t3;
@@ -146,8 +154,8 @@ int main()
 	Sprite patratMancare(t3);
 
     sf:Clock clock;
-    m.x=rand()%length;
-    m.y=rand()%width;
+    m.x=abs(rand())%length;
+    m.y=abs(rand())%width;
     //inteligent
     dir=inteligent();
 
